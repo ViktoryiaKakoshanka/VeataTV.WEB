2ad33e5986c3b753b24f95554affc468ec0de67f
Viktoryia <Viktoryia_Kakoshanka@epam.com>
2020-01-13 18:46:11 +0300

    Last comment

---
+++ WebVestaTVCable/Controllers/Admin/MasterController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebVestaTVCable.Controllers.Admin
+{
+    public class MasterController : Controller
+    {
+        // GET: Master
+        public ActionResult Index()
+        {
+            var url = "https://localhost:44316/master";
+
+            var request = WebRequest.Create(url);
+            return View();
+        }
+
+        // GET: Master/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Master/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Master/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Master/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Master/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Master/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Master/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebVestaTVCable/Models/MasterViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VestaTV.Cabel.Core.Models;
+
+namespace WebVestaTVCable.Models
+{
+    public class MasterViewModel
+    {
+        List<Master> Masters;
+
+    }
+}
